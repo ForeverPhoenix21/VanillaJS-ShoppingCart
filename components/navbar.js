b980c99292a0991506410850a41693ad4490d4e4
ForeverPhoenix21 <Derek.Diaz86@gmail.com>
2023-04-06 13:34:30 -0400

    Set up the file structure to use components. Created the navbar and navbar componenet. Styled in navbar.css

---
+++ components/navbar.js
@@ -0,0 +1,31 @@
+const handleNavbar = document.getElementById("navbar");
+
+let navbar = () => {
+  return (handleNavbar.innerHTML = `
+  <div class="navbar-container">
+    <div class="navbar-logo">
+      <a href="/index.html">
+        <img
+          style="max-height: 50px"
+          src="/assets/images/SPOILER_2ffnww.jpg"
+          alt="Stella's Market brand logo"
+        />
+        <p>Stellas Market</p>
+      </a>
+    </div>
+    <div class="navbar-links">
+      <ul>
+        <li><a href="#">Products</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+
+      <div class="cart">
+        <i class="bi bi-cart4"></i>
+        <div class="cart-count">0</div>
+      </div>
+    </div>
+  </div>`);
+};
+
+navbar()
