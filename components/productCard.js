a3ec0a37d300f3ce9e54c39281623063c75dc378
ForeverPhoenix21 <Derek.Diaz86@gmail.com>
2023-04-12 13:17:20 -0400

    Created a variable called Products which is an array that currently holds all the prodcut information for the store. Products array is mapped and destructured in the function geerateProdcutCard.

--- components/productCard.js
+++ components/productCard.js
@@ -3,31 +3,79 @@ let getShop = document.getElementById("shop-container");
 
 let products = [
   {
-    id: "Mens_shirt",
-    name: "A Black Shirt",
-    desc: "A description of a mens shirt",
+    id: "Mens_shirt_ss",
+    color:"Black",
+    name: "El Capitan",
+    quantity: 10,
+    price: 30,
+    desc: "Classic Fit Logo Tee",
+    img: "/assets/apperal_images/alex-haigh--r67bw_KBWA-unsplash.jpg",
+    alt: "A picture of male model in the Mens Graphic Tee",
+  },
+  {
+    id: "Womens_shirt_ls",
+    name: "Lucky Lady",
+    color: "White",
     price: 45,
-    img: "assets/apperal_images/alex-haigh--r67bw_KBWA-unsplash.jpg",
+    desc: " Women's Long Sleeve",
+    img: "/assets/apperal_images/alex-haigh-CBn0QAL2q1A-unsplash.jpg",
+    alt: "Nothing to describe",
+  },
+  {
+    id: "Mens_shirt_ss",
+    name: "Rock Style",
+    color: "White",
+    price: 30,
+    desc: "Classic Fit Logo Tee",
+    img: "assets/apperal_images/alex-haigh-fUxGacFLsC0-unsplash.jpg",
+    alt: "Nothing to describe",
+  },
+  {
+    id: "Mens_shirt_ls",
+    name: "Calivera",
+    color: "White",
+    price: 45,
+    desc: "Classic Fit Long-Sleeve",
+    img: "assets/apperal_images/alex-haigh-S9y9Z5gTWGY-unsplash.jpg",
+    alt: "Nothing to describe",
+  },
+  {
+    id: "Mens_shirt_ls",
+    name: "Triple Down",
+    color: "White",
+    price: 45,
+    desc: "Classic Fit Long-Sleeve",
+    img: "assets/apperal_images/alex-haigh-uHTSYUwXjLM-unsplash.jpg",
+    alt: "Nothing to describe",
+  },  {
+    id: "Mens_hoodie",
+    name: "Mighty Hoodie",
+    color: "White",
+    price: 60,
+    desc: "Mens / Women's Hoodie",
+    img: "assets/apperal_images/alex-haigh-zrp9Iu5bF1w-unsplash.jpg",
+    alt: "Nothing to describe",
   },
 ];
 
-
-
 const generateProductCard = () => {
-  return (getShop.innerHTML = `
-  <div class="item">
+  return (getShop.innerHTML = products
+    .map((product) => {
+      let { id, name, desc, price, img, alt } = product;
+      return `
+  <div class="item" id = "product-id-" ${id}>
   <img
   style="max-height: 250px"
-    src="./assets/apperal_images/alex-haigh--r67bw_KBWA-unsplash.jpg"
+    src=${img} alt=${alt}
   />
   <div class="item-name">
-    <h3>Product title</h3>
+    <h3>${name}</h3>
   </div>
   <div class="item-description">
-    <p>This is the destription</p>
+    <p>${desc}</p>
   </div>
   <div class="price-quantity">
-    <div class="price">$45</div>
+    <div class="price">$${price}</div>
     <div class="button">
     <button><i class="bi bi-dash-circle"></i></button>
     <div class="quantity">0</div>
@@ -35,9 +83,9 @@ const generateProductCard = () => {
   </div>
   </div>
 </div>
-`);
+`;
+    })
+    .join(""));
 };
-
-
 
 generateProductCard();
